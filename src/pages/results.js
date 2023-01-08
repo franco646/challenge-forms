86a7c7f0f596c0bf4f763de8a7acfd1a3c630194
franco646 <franco@>
2023-01-08 12:17:21 -0300

    Update results.js

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { doc, getDoc } from "firebase/firestore";
+import { useParams } from "react-router-dom";
+
+import db from "../firebase/db";
 
 const ResultsPage = () => {
   const [user, setUser] = useState({});
+  let { userId } = useParams();
 
   useEffect(() => {
     const fetchUser = async () => {
-      const { data } = await axios.get("https://us-central1-forms-challenge.cloudfunctions.net/app/data");
-      setUser(data.user);
+      const docRef = doc(db, "users", userId);
+      const docSnap = await getDoc(docRef);
+      setUser(docSnap.data());
     };
     fetchUser();
-  }, []);
+  }, [userId]);
 
   return (
     <ul>
-        <li>{user.full_name}</li>
-        <li>{user.email}</li>
-        <li>{user.birth_date}</li>
-        <li>{user.country_of_origin}</li>
-        <li>{user.terms_and_conditions}</li>
+      <li>{user.full_name}</li>
+      <li>{user.email}</li>
+      <li>{user.birth_date}</li>
+      <li>{user.country_of_origin}</li>
+      <li>{user.terms_and_conditions}</li>
     </ul>
-  )
+  );
 };
 
 export default ResultsPage;
